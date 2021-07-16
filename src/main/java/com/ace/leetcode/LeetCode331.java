756ad878ca0e86b0d0f74cd941cffc4e0ad5520b
amdad.choudhury <amdad.choudhury@outlook.com>
2021-07-16 12:55:23 +0100

    LeetCode 331.

--- src/main/java/com/ace/leetcode/LeetCode331.java
+++ src/main/java/com/ace/leetcode/LeetCode331.java
@@ -4,12 +4,13 @@ public class LeetCode331
 {
     public boolean verify(String[] input)
     {
+        boolean childFound = false;
         int currentParentIndex = 0;
         for (int i = 0; i < input.length; i++)
         {
             if (input[i].equals("#"))
             {
-                if (input[currentParentIndex].equals("#"))
+                if (input[currentParentIndex].equals("#") || !childFound)
                 {
                     return false;
                 }
@@ -17,6 +18,10 @@ public class LeetCode331
             else
             {
                 currentParentIndex = i;
+                if (i > 0)
+                {
+                    childFound = true;
+                }
             }
         }
         return true;
--- src/test/java/com/ace/leetcode/LeetCode331Test.java
+++ src/test/java/com/ace/leetcode/LeetCode331Test.java
@@ -2,6 +2,7 @@ package com.ace.leetcode;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class LeetCode331Test
@@ -9,8 +10,8 @@ public class LeetCode331Test
     @Test
     public void shouldCreateBinaryTreeFromArray()
     {
-        assertTrue(new LeetCode331().verify(new String[]{"1", "#"}));
-        assertTrue(new LeetCode331().verify(new String[]{"9", "#", "#", "1"}));
+        assertFalse(new LeetCode331().verify(new String[]{"1", "#"}));
+        assertFalse(new LeetCode331().verify(new String[]{"9", "#", "#", "1"}));
         assertTrue(new LeetCode331().verify(new String[]{"9", "3", "4", "#", "#", "1", "#", "#", "2", "#", "6", "#", "#"}));
     }
 }
