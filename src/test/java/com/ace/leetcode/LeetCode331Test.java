eead16a997ffff9d19b13be82bfd093406c238f3
rudrajyotib <Gingerbread123!>
2021-07-16 19:44:29 +0530

    LeetCode 331 : added test cases to fix.

--- src/test/java/com/ace/leetcode/LeetCode331Test.java
+++ src/test/java/com/ace/leetcode/LeetCode331Test.java
@@ -14,6 +14,7 @@ public class LeetCode331Test
         assertFalse(new LeetCode331().verify(new String[]{"1","2","3","#","#","#"}));
         assertFalse(new LeetCode331().verify(new String[]{"1", "#"}));
         assertFalse(new LeetCode331().verify(new String[]{"9", "#", "#", "1"}));
-        assertTrue(new LeetCode331().verify(new String[]{"9", "3", "4", "#", "#", "1", "#", "#", "2", "#", "6", "#", "#"}));
+        assertFalse(new LeetCode331().verify(new String[]{"9", "3", "4", "#", "#", "1", "#", "#", "2", "#", "#","#","1","1", "6", "#", "#"}));
+        assertFalse(new LeetCode331().verify(new String[]{"1","2","4","#","#","5","#","#","3","6","#","#","7","#","#","8","#"}));
     }
 }
