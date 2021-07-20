b97b117047f549c8015ae33b6f6bb07829acefa9
amdad.choudhury <amdad.choudhury@outlook.com>
2021-07-20 15:02:55 +0100

    LeetCode 331.

--- src/main/java/com/ace/leetcode/LeetCode331.java
+++ src/main/java/com/ace/leetcode/LeetCode331.java
@@ -4,8 +4,7 @@ public class LeetCode331
 {
     public boolean verify(String[] input)
     {
-        int countOpenNodes = 0;
-        boolean first = true;
+        int countOpenNodes = 1;
         int countOfDeadNodesInCurrentSequence = 0;
         for (String node : input)
         {
@@ -21,19 +20,11 @@ public class LeetCode331
             else
             {
                 countOfDeadNodesInCurrentSequence = 0;
-                if (first)
+                if (countOpenNodes == 0)
                 {
-                    countOpenNodes += 2;
-                    first = false;
+                    return false;
                 }
-                else
-                {
-                    if (countOpenNodes == 0)
-                    {
-                        return false;
-                    }
-                    countOpenNodes += 1;
-                }
+                countOpenNodes += 1;
             }
         }
         return countOpenNodes == 0;
