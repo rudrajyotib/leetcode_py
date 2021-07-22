4a25c3a1d3cbbef88d7d69a7bd6cba91a01a6a70
ella-pc <amdad.choudhury@outlook.com>
2021-07-22 14:00:37 +0100

    105.

--- src/main/java/com/ace/leetcode/LeetCode105.java
+++ src/main/java/com/ace/leetcode/LeetCode105.java
@@ -1,7 +1,37 @@
 package com.ace.leetcode;
 
+import com.ace.leetcode.utils.BinaryTree;
+
 public class LeetCode105
 {
-
-
+	
+	public static BinaryTree create(int[] preorder, int[] inorder)
+	{
+		int lastIndex = preorder.length - 1;
+		return buildTree(preorder, 0, lastIndex, inorder, 0);
+	}
+	
+	private static BinaryTree buildTree(int[] preorder,
+										int preorderStartIndex,
+										int preorderEndIndex,
+										int[] inorder,
+										int inorderStartIndex)
+	{
+		if (preorderStartIndex > preorderEndIndex)
+		{
+			return null;
+		}
+		
+		int indexOfCurrentRootValueInInorderArray = 0;
+		while (inorder[indexOfCurrentRootValueInInorderArray] != preorder[preorderStartIndex])
+		{
+			indexOfCurrentRootValueInInorderArray++;
+		}
+		
+		int tnel = indexOfCurrentRootValueInInorderArray - inorderStartIndex;
+		BinaryTree left = buildTree(preorder, preorderStartIndex + 1, preorderStartIndex + tnel, inorder, inorderStartIndex);
+		BinaryTree right = buildTree(preorder, preorderStartIndex + tnel + 1, preorderEndIndex, inorder, indexOfCurrentRootValueInInorderArray + 1);
+		return new BinaryTree(preorder[preorderStartIndex], left, right);
+	}
+	
 }
--- src/test/java/com/ace/leetcode/LeetCode105Test.java
+++ src/test/java/com/ace/leetcode/LeetCode105Test.java
@@ -1,14 +1,42 @@
 package com.ace.leetcode;
 
+import com.ace.leetcode.utils.BinaryTree;
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 public class LeetCode105Test
 {
 	@Test
 	public void shouldCreateBinaryTreeFromArray()
 	{
-		assertTrue(new LeetCode331().verify(new String[]{"1", "2", "4", "#", "#", "5", "#", "#", "3", "6", "#", "#", "7", "#", "#"}));
+		/*
+		 * Always get node values from the preorder array.
+		 * But cannot search beyond the index of where the root is in the inorder array.
+		 * Preorder tells us 3 is the root node and 3 is in index 1 of inorder.
+		 * Therefore left's children must be within 0-1 index of preorder array.
+		 * But left is in index 1 hence left node cannot have children.
+		 * Everything to the right of 3 in inorder are right nodes' child nodes.
+		 */
+		
+		
+		BinaryTree root = LeetCode105.create(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7});
+		assertEquals(root.getValue(), 3);
+		assertEquals(root.getLeft().getValue(), 9);
+		assertEquals(root.getRight().getValue(), 20);
+		assertNull(root.getLeft().getLeft());
+		assertNull(root.getLeft().getRight());
+		assertEquals(root.getRight().getLeft().getValue(), 15);
+		assertEquals(root.getRight().getRight().getValue(), 7);
+	}
+	
+	@Test
+	public void shouldCreateBinaryTreeSingleValue()
+	{
+		BinaryTree root = LeetCode105.create(new int[]{-1}, new int[]{-1});
+		assertEquals(root.getValue(), -1);
+		assertNull(root.getLeft());
+		assertNull(root.getRight());
 	}
 }
