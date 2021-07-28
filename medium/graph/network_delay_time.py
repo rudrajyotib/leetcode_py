29c69e4c6a913029f40ce1597eb3f81f6e32fdbc
rudrajyotib <Gingerbread123!>
2021-07-28 21:02:52 +0530

    https://leetcode.com/problems/network-delay-time/ Update

--- medium/graph/network_delay_time.py
+++ medium/graph/network_delay_time.py
@@ -6,24 +6,22 @@ class Solution:
     # noinspection PyMethodMayBeStatic
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         adjacency_list: Dict[int, List[Tuple]] = {}
-        n = n - 1
-        k = k - 1
         for time in times:
             if (time[0] - 1) in adjacency_list:
                 adjacency_list[(time[0] - 1)].append(((time[1] - 1), time[2]))
             else:
                 adjacency_list[(time[0] - 1)] = [((time[1] - 1), time[2])]
 
-        if k not in adjacency_list:
+        if k - 1 not in adjacency_list:
             return -1
 
-        relaxed_nodes: List[bool] = [False] * (n + 1)
-        visited_nodes: List[bool] = [False] * (n + 1)
-        shortest_distance: List = [math.inf] * (n + 1)
-        visited_nodes[k] = True
-        shortest_distance[k] = 0
+        relaxed_nodes: List[bool] = [False] * n
+        visited_nodes: List[bool] = [False] * n
+        shortest_distance: List = [math.inf] * n
+        visited_nodes[k - 1] = True
+        shortest_distance[k - 1] = 0
 
-        adjacent_nodes = adjacency_list[k]
+        adjacent_nodes = adjacency_list[k - 1]
         for adjacent_node in adjacent_nodes:
             relaxed_nodes[adjacent_node[0]] = True
             shortest_distance[adjacent_node[0]] = adjacent_node[1]
@@ -31,7 +29,7 @@ class Solution:
         def shortest_relax_node() -> int:
             shorted_distance_temp = math.inf
             shorted_distance_index = 0
-            for i in range(0, n + 1, 1):
+            for i in range(0, n, 1):
                 if relaxed_nodes[i] and shortest_distance[i] < shorted_distance_temp:
                     shorted_distance_index = i
                     shorted_distance_temp = shortest_distance[i]
