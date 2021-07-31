7173587143e1c10f2c988a6fddbac4056103fa63
rudrajyotib <Gingerbread123!>
2021-07-31 08:47:22 +0530

    https://leetcode.com/problems/number-of-operations-to-make-network-connected/ Update

--- medium/graph/operations_to_make_network_connected.py
+++ medium/graph/operations_to_make_network_connected.py
@@ -15,17 +15,17 @@ class Solution:
             adjacency_list[connection[0]].append(connection[1])
             adjacency_list[connection[1]].append(connection[0])
 
-        def visit_nodes_dfs(node: int):
+        def visit_nodes_dfs(node: int, adj_list: List[List[int]]):
             if not visited_nodes[node]:
                 visited_nodes[node] = True
-                for adjacent_node in adjacency_list[node]:
-                    visit_nodes_dfs(adjacent_node)
+                for adjacent_node in adj_list[node]:
+                    visit_nodes_dfs(node=adjacent_node, adj_list=adj_list)
 
         group_counter = 0
 
         for node_index in range(n):
             if not visited_nodes[node_index]:
                 group_counter += 1
-                visit_nodes_dfs(node=node_index)
+                visit_nodes_dfs(node=node_index, adj_list=adjacency_list)
 
         return group_counter - 1
