1fb32ba6917e9f00b80f35d55fd27f19471cd96e
rudrajyotib <Gingerbread123!>
2021-08-01 11:07:33 +0530

    https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/ Update

--- medium/graph/reorder_routes_to_make_all_paths_towards_orig_city.py
+++ medium/graph/reorder_routes_to_make_all_paths_towards_orig_city.py
@@ -5,12 +5,11 @@ from typing import List, Dict, Set
 class Solution:
     reverse_counter: int
 
+    # noinspection PyMethodMayBeStatic
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         Solution.reverse_counter = 0
         adjacency_matrix: Dict[int, Set[int]] = collections.defaultdict(set)
         adjacency_list: Dict[int, List[int]] = collections.defaultdict(list)
-        # for row in range(n):
-        #    adjacency_matrix.append([-1] * n)
         for connection in connections:
             adjacency_matrix[connection[0]].add(connection[1])
             adjacency_list[connection[0]].append(connection[1])
@@ -18,30 +17,19 @@ class Solution:
 
         visited: List[bool] = [False] * n
         adjacency_matrix[0].add(0)
-        # bfs_queue = collections.deque()
-        # bfs_queue.append(0)
 
-        def find_paths_dfs(destination: int, source: int):
-            visited[source] = True
-            if destination not in adjacency_matrix[source]:
-                Solution.reverse_counter += 1
-            for src in adjacency_list[source]:
-                if not visited[src]:
-                    find_paths_dfs(destination=source, source=src)
+        def find_paths_bfs():
+            dfs_queue = collections.deque()
+            dfs_queue.append((0, 0))
+            while dfs_queue:
+                node = dfs_queue.popleft()
+                visited[node[0]] = True
+                if node[0] not in adjacency_matrix[node[1]]:
+                    Solution.reverse_counter += 1
+                for src in adjacency_list[node[1]]:
+                    if not visited[src]:
+                        dfs_queue.append((node[1], src))
 
-        # while len(bfs_queue) > 0:
-        #     node = bfs_queue.popleft()
-        #     for column in range(0, n, 1):
-        #         if adjacency_matrix[node][column] == 1:
-        #             bfs_queue.append(column)
-        #             adjacency_matrix[column][node] = -1
-        #             adjacency_matrix[node][column] = -1
-        #         elif adjacency_matrix[node][column] == 2:
-        #             reverse_counter += 1
-        #             bfs_queue.append(column)
-        #             adjacency_matrix[column][node] = -1
-        #             adjacency_matrix[node][column] = -1
-
-        find_paths_dfs(destination=0, source=0)
+        find_paths_bfs()
 
         return Solution.reverse_counter
