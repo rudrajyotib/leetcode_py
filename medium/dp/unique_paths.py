be7e8c738e2f63598ff21b3b05e9eec47fa0a1da
rudrajyotib <Gingerbread123!>
2021-08-04 00:34:58 +0530

    https://leetcode.com/problems/unique-paths/ Update

--- medium/dp/unique_paths.py
+++ medium/dp/unique_paths.py
@@ -10,12 +10,9 @@ class Solution:
             return 1
         if n == 1:
             return 1
-        result: List[List[int]] = [[0 for _ in range(n)] for _ in range(m)]
-        result[0][0] = 1
-        for i in range(1, n, 1):
-            result[0][i] += result[0][i - 1]
+        result: List[List[int]] = [[1 for _ in range(n)]]
         for i in range(1, m, 1):
-            result[i][0] += result[i - 1][0]
+            result.append([1 if _ == 0 else 0 for _ in range(n)])
         for outer_index in range(1, m, 1):
             for inner_index in range(1, n, 1):
                 result[outer_index][inner_index] = result[outer_index - 1][inner_index] + result[outer_index][
