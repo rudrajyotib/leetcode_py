5075a5f2c388438b45a3087f5c4942209175c000
rudrajyotib <Gingerbread123!>
2021-08-06 12:23:43 +0530

    https://leetcode.com/problems/largest-divisible-subset/ test case updated.

--- medium/dp/test_largest_divisible_subset.py
+++ medium/dp/test_largest_divisible_subset.py
@@ -6,9 +6,10 @@ from medium.dp.largest_divisible_subset import Solution
 class TestSolution(TestCase):
     def test_largest_divisible_subset(self):
         solution = Solution()
-        self.assertEqual([2, 4, 8], solution.largestDivisibleSubset(nums=[2, 3, 4, 9, 8]))
-        self.assertEqual([1, 3], solution.largestDivisibleSubset(nums=[1, 3]))
-        self.assertEqual([1, 3, 6], solution.largestDivisibleSubset(nums=[1, 3, 6, 9]))
-        self.assertEqual([1, 3, 9, 27], solution.largestDivisibleSubset(nums=[1, 3, 27, 9]))
+        self.assertEqual({2, 4, 8}, solution.largestDivisibleSubset(nums=[2, 3, 4, 9, 8]))
+        self.assertEqual({1, 3}, solution.largestDivisibleSubset(nums=[1, 3]))
+        self.assertEqual({1, 3, 6}, solution.largestDivisibleSubset(nums=[1, 3, 6, 9]))
+        self.assertEqual({1, 3, 9, 27}, solution.largestDivisibleSubset(nums=[1, 3, 27, 9]))
         self.assertEqual([1], solution.largestDivisibleSubset(nums=[1]))
-        self.assertEqual([1, 2, 4, 8, 24, 72], solution.largestDivisibleSubset(nums=[1, 2, 3, 4, 6, 8, 12, 24, 72]))
+        self.assertEqual({1, 2, 4, 8, 24, 72}, solution.largestDivisibleSubset(nums=[1, 2, 3, 4, 6, 8, 12, 24, 72]))
+        self.assertEqual({ 4, 8, 16}, solution.largestDivisibleSubset(nums=[3, 4, 16, 8]))
