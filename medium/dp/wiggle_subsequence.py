566c17acea5b3e9afa5ee7fef7195fdac93a43e2
rudrajyotib <Gingerbread123!>
2021-08-07 08:28:42 +0530

    https://leetcode.com/problems/wiggle-subsequence/ O(n) - updated

--- medium/dp/wiggle_subsequence.py
+++ medium/dp/wiggle_subsequence.py
@@ -2,18 +2,16 @@ from typing import List, Tuple
 
 
 class Solution:
+    # noinspection PyMethodMayBeStatic
     def wiggleMaxLength(self, nums: List[int]) -> int:
         length = len(nums)
         derivation: List[Tuple] = [(1, 1) for _ in range(0, len(nums))]
-        for outer_index in range(0, length, 1):
-            for inner_index in range(outer_index + 1, length, 1):
-                if nums[inner_index] > nums[outer_index]:
-                    derivation[inner_index] = (
-                        max(derivation[outer_index][1] + 1, derivation[inner_index][0]),
-                        derivation[inner_index][1])
-                elif nums[inner_index] < nums[outer_index]:
-                    derivation[inner_index] = (
-                        derivation[inner_index][0],
-                        max(derivation[outer_index][0] + 1, derivation[inner_index][1]))
+        for index in range(1, length, 1):
+            if nums[index] > nums[index - 1]:
+                derivation[index] = (derivation[index - 1][1] + 1, derivation[index - 1][1])
+            elif nums[index] < nums[index - 1]:
+                derivation[index] = (derivation[index - 1][0], derivation[index - 1][0] + 1)
+            else:
+                derivation[index] = derivation[index-1]
 
-        return max(derivation[length-1])
+        return max(derivation[length - 1])
