df563fe81a1966e61761a0457e92f523bc5a7d39
rudrajyotib <Gingerbread123!>
2021-08-10 19:46:29 +0530

    https://leetcode.com/problems/01-matrix/ Accepted - performance updated.

--- medium/dp/zero_one_matrix.py
+++ medium/dp/zero_one_matrix.py
@@ -1,3 +1,4 @@
+import collections
 from typing import List
 
 
@@ -6,6 +7,8 @@ class Solution:
         row_limit = len(mat)
         col_limit = len(mat[0])
         result = [[-1 for _ in range(col_limit)] for _ in range(row_limit)]
+        enqueued = [[False for _ in range(col_limit)] for _ in range(row_limit)]
+        queue = collections.deque()
 
         def update_recursive(start_row: int, start_col: int, path_length: int):
             if 0 <= start_row < row_limit and 0 <= start_col < col_limit:
@@ -18,13 +21,29 @@ class Solution:
                     update_recursive(start_row - 1, start_col, result[start_row][start_col] + 1)
                     update_recursive(start_row + 1, start_col, result[start_row][start_col] + 1)
 
+        def update_association(start_row: int, start_col: int):
+            if 0 <= start_row < row_limit and 0 <= start_col < col_limit:
+                if mat[start_row][start_col] == 0:
+                    return
+                if enqueued[start_row][start_col]:
+                    return
+                queue.append((start_row, start_col))
+                result[start_row][start_col] = 1
+                enqueued[start_row][start_col] = True
+
         for row_index in range(row_limit):
             for col_index in range(col_limit):
                 if mat[row_index][col_index] == 0:
                     result[row_index][col_index] = 0
-                    update_recursive(row_index, col_index - 1, 1)
-                    update_recursive(row_index, col_index + 1, 1)
-                    update_recursive(row_index - 1, col_index, 1)
-                    update_recursive(row_index + 1, col_index, 1)
+                    update_association(row_index, col_index - 1)
+                    update_association(row_index, col_index + 1)
+                    update_association(row_index - 1, col_index)
+                    update_association(row_index + 1, col_index)
+        while queue:
+            cell = queue.popleft()
+            update_recursive(cell[0] + 1, cell[1], 2)
+            update_recursive(cell[0] - 1, cell[1], 2)
+            update_recursive(cell[0], cell[1] + 1, 2)
+            update_recursive(cell[0], cell[1] - 1, 2)
 
         return result
