2fe4ee4c0290b6a3ee25e5b85a89f1b59fa3896d
ella-pc <amdad.choudhury@outlook.com>
2021-08-11 16:48:06 +0100

    LeetCode912

--- src/main/java/com/ace/leetcode/LeetCode207.java
+++ src/main/java/com/ace/leetcode/LeetCode207.java
@@ -1,12 +1,33 @@
 package com.ace.leetcode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LeetCode207
 {
+	public static int[] findOrder(int[][] prerequisites)
+	{
+		List<Integer> schedule = new ArrayList<>();
+		for (int[] course : prerequisites)
+		{
+			if (course.length > 0)
+			{
+				if (!schedule.contains(course[1]))
+				{
+					schedule.add(course[1]);
+				}
+				if (!schedule.contains(course[0]))
+				{
+					schedule.add(course[0]);
+				}
+			}
+		}
+		if (schedule.size() == 0)
+		{
+			return new int[]{};
+		}
+		return schedule.stream().mapToInt(Number::intValue).toArray();
+	}
+	
 	public static boolean solution(int[][] prerequisites)
 	{
 		Map<Integer, Set<Integer>> courses = new HashMap<>();
--- src/test/java/com/ace/leetcode/LeetCode207Test.java
+++ src/test/java/com/ace/leetcode/LeetCode207Test.java
@@ -2,8 +2,7 @@ package com.ace.leetcode;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class LeetCode207Test
 {
@@ -21,5 +20,14 @@ public class LeetCode207Test
 		assertFalse(LeetCode207.solution(new int[][]{{1, 0}, {0, 1}}));
 		assertFalse(LeetCode207.solution(new int[][]{{0, 10}, {3, 18}, {5, 5}, {6, 11}, {11, 14}, {13, 1}, {15, 1}, {17, 4}}));
 		assertFalse(LeetCode207.solution(new int[][]{{1, 0}, {0, 2}, {2, 1}}));
+	}
+	
+	@Test
+	public void shouldReturnSchedule()
+	{
+		assertArrayEquals(new int[]{}, LeetCode207.findOrder(new int[][]{{}}));
+		assertArrayEquals(new int[]{0, 1}, LeetCode207.findOrder(new int[][]{{1, 0}}));
+		assertArrayEquals(new int[]{1, 0}, LeetCode207.findOrder(new int[][]{{0, 1}}));
+		assertArrayEquals(new int[]{4, 1, 2, 3}, LeetCode207.findOrder(new int[][]{{1, 4}, {2, 4}, {3, 1}, {3, 2}}));
 	}
 }
