e93dd05cda0d8b62cd4a608af0c22cf112d80f66
ella-pc <amdad.choudhury@outlook.com>
2021-08-17 18:34:56 +0100

    Adjacency matrix.

---
+++ src/main/java/com/ace/leetcode/utils/AdjacencyMatrix.java
@@ -0,0 +1,42 @@
+package com.ace.leetcode.utils;
+
+public class AdjacencyMatrix
+{
+	public static int[][] countPaths(int[][] matrix)
+	{
+		int[][] result = multiply(multiply(matrix));
+		print(result);
+		return result;
+	}
+	
+	private static int[][] multiply(int[][] matrix)
+	{
+		int[][] result = new int[matrix.length][matrix[0].length];
+		
+		for (int i = 0; i < matrix.length; i++)
+		{
+			for (int j = 0; j < matrix[0].length; j++)
+			{
+				result[i][j] = 0;
+				for (int k = 0; k < matrix[0].length; k++)
+				{
+					result[i][j] += matrix[i][k] * matrix[k][j];
+				}
+			}
+		}
+		return result;
+	}
+	
+	private static void print(int[][] matrix)
+	{
+		for (int[] ints : matrix)
+		{
+			for (int j = 0; j < matrix[0].length; j++)
+			{
+				
+				System.out.print(ints[j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ src/test/java/com/ace/leetcode/utils/AdjacencyMatrixTest.java
@@ -0,0 +1,22 @@
+package com.ace.leetcode.utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AdjacencyMatrixTest
+{
+	@Test
+	public void shouldReturnFurthestPath()
+	{
+		assertEquals(new int[][]{{1, 0, 0}, {1, 0, 0}, {0, 0, 1}}, AdjacencyMatrix.countPaths(new int[][]{{0, 0, 1}, {0, 0, 1}, {1, 0, 0}}));
+		assertEquals(new int[][]{{1, 1, 0}, {0, 0, 0}, {0, 0, 1}}, AdjacencyMatrix.countPaths(new int[][]{{0, 0, 1}, {0, 0, 0}, {1, 1, 0}}));
+		assertEquals(new int[][]{{0, 1, 0}, {0, 1, 0}, {0, 0, 1}}, AdjacencyMatrix.countPaths(new int[][]{{0, 0, 1}, {0, 0, 1}, {0, 1, 0}}));
+	}
+	
+	@Test
+	public void shouldReturnTotalNumberOfPathsBetweenNodes()
+	{
+		assertEquals(new int[][]{{1, 2, 2, 6, 1}, {1, 0, 4, 3, 2}, {3, 0, 2, 3, 3}, {1, 1, 4, 5, 2}, {1, 1, 1, 3, 1}}, AdjacencyMatrix.countPaths(new int[][]{{0, 1, 0, 1, 0}, {0, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {0, 0, 1, 1, 0}, {0, 0, 0, 1, 0}}));
+	}
+}
