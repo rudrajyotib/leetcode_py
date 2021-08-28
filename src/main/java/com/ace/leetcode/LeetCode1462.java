650085330147caf875a9c0836d4b97e6c02434c2
ella-pc <amdad.choudhury@outlook.com>
2021-08-28 17:46:55 +0100

    leetcode 1462 using Floyd Warshall.

--- src/main/java/com/ace/leetcode/LeetCode1462.java
+++ src/main/java/com/ace/leetcode/LeetCode1462.java
@@ -1,5 +1,67 @@
 package com.ace.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode1462
 {
+	public static List<Boolean> checkIfPrerequisiteUsingFloydWarshall(int numCourses, int[][] prerequisites, int[][] queries)
+	{
+		List<Boolean> result = new ArrayList<>();
+		if (prerequisites.length == 0)
+		{
+			for (int i = 0; i < numCourses; i++)
+			{
+				result.add(false);
+			}
+			return result;
+		}
+		
+		Boolean[][] graph = createGraph(numCourses);
+		for (int[] prerequisite : prerequisites)
+		{
+			graph[prerequisite[0]][prerequisite[1]] = true;
+		}
+		
+		for (int k = 0; k < numCourses; k++)
+		{
+			for (int i = 0; i < numCourses; i++)
+			{
+				for (int j = 0; j < numCourses; j++)
+				{
+					if (graph[i][k] != null && graph[k][j] != null && (graph[i][k] && graph[k][j]))
+					{
+						graph[i][j] = true;
+					}
+				}
+			}
+		}
+		
+		for (int[] query : queries)
+		{
+			result.add(graph[query[0]][query[1]]);
+		}
+		
+		return result;
+	}
+	
+	private static Boolean[][] createGraph(int numCourses)
+	{
+		Boolean[][] graph = new Boolean[numCourses][numCourses];
+		for (int i = 0; i < numCourses; i++)
+		{
+			for (int j = 0; j < numCourses; j++)
+			{
+				if (i == j)
+				{
+					graph[i][j] = true;
+				}
+				else
+				{
+					graph[i][j] = false;
+				}
+			}
+		}
+		return graph;
+	}
 }
--- src/test/java/com/ace/leetcode/LeetCode1462Test.java
+++ src/test/java/com/ace/leetcode/LeetCode1462Test.java
@@ -1,5 +1,16 @@
 package com.ace.leetcode;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LeetCode1462Test
 {
+	@Test
+	public void shouldReturnPrerequisites()
+	{
+		assertEquals(new Boolean[]{false, false}, LeetCode1462.checkIfPrerequisiteUsingFloydWarshall(2, new int[0][0], new int[][]{{0, 1}, {1, 0}}).toArray(new Boolean[0]));
+		assertEquals(new Boolean[]{false, true}, LeetCode1462.checkIfPrerequisiteUsingFloydWarshall(2, new int[][]{{1, 0}}, new int[][]{{0, 1}, {1, 0}}).toArray(new Boolean[0]));
+		assertEquals(new Boolean[]{true, true}, LeetCode1462.checkIfPrerequisiteUsingFloydWarshall(3, new int[][]{{1, 2}, {1, 0}, {2, 0}}, new int[][]{{1, 0}, {1, 2}}).toArray(new Boolean[0]));
+	}
 }
