7731fe842a9c80097d81745cfcf389667c2f6da2
Rudrajyoti Biswas <rudrajyoti.biswas@outlook.com>
2022-07-08 15:45:45 +0530

    https://www.interviewbit.com/problems/inorder-traversal-of-cartesian-tree

---
+++ src/main/java/interviewbit/solutions/InorderTraversalOfCartesianTree.java
@@ -0,0 +1,125 @@
+package interviewbit.solutions;
+
+/*
+https://www.interviewbit.com/problems/inorder-traversal-of-cartesian-tree
+ */
+
+
+
+public class InorderTraversalOfCartesianTree {
+
+    public TreeNode solve(int[] inOrderTraversal) {
+        return new Solution().buildTree(inOrderTraversal);
+    }
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+            left = null;
+            right = null;
+        }
+    }
+
+
+    public class Solution {
+        public TreeNode buildTree(int[] inOrderTraversal) {
+
+            MaxSegmentTree segmentTree = new MaxSegmentTree(inOrderTraversal);
+            return buildTreeRecursive(0, inOrderTraversal.length - 1, inOrderTraversal, segmentTree);
+
+        }
+
+
+        public TreeNode buildTreeRecursive(int leftLimit, int rightLimit, int[] inOrderTraversal, MaxSegmentTree segmentTree) {
+            if (leftLimit > rightLimit) {
+                return null;
+            }
+            int maxElementIndexInRange = segmentTree.query(leftLimit, rightLimit);
+            TreeNode node = new TreeNode(inOrderTraversal[maxElementIndexInRange]);
+            node.right = buildTreeRecursive(maxElementIndexInRange + 1, rightLimit, inOrderTraversal, segmentTree);
+            node.left = buildTreeRecursive(leftLimit, maxElementIndexInRange - 1, inOrderTraversal, segmentTree);
+            return node;
+        }
+
+
+        class MaxSegmentTree {
+            private final int[] elements;
+            private final int[] segments;
+
+            MaxSegmentTree(int[] elements) {
+                this.elements = elements;
+                this.segments = new int[calculateTotalCells(elements.length)];
+                buildSegmentTree();
+            }
+
+            int calculateTotalCells(int count) {
+                int leafNodesCount = 1;
+                int totalCells = 1;
+                while (leafNodesCount < count) {
+                    leafNodesCount *= 2;
+                    totalCells += leafNodesCount;
+                }
+                return totalCells;
+            }
+
+            int query(int startIndex, int endIndex) {
+                return queryRecursive(0, elements.length - 1, startIndex, endIndex, 0);
+            }
+
+            int queryRecursive(int leftSubTreeLimit, int rightSubTreeLimit, int leftQueryIndex, int rightQueryIndex, int cellReferenceIndex) {
+                if (leftSubTreeLimit > rightQueryIndex || rightSubTreeLimit < leftQueryIndex) {
+                    return -1;
+                }
+                if (leftSubTreeLimit >= leftQueryIndex && rightSubTreeLimit <= rightQueryIndex) {
+                    return segments[cellReferenceIndex];
+                }
+                int midSubTreeLimit = (leftSubTreeLimit + rightSubTreeLimit) / 2;
+                int indexFromLeftSubTreeSearch = queryRecursive(leftSubTreeLimit, midSubTreeLimit, leftQueryIndex, rightQueryIndex, (cellReferenceIndex * 2 + 1));
+                int indexFromRightSubTreeSearch = queryRecursive(midSubTreeLimit + 1, rightSubTreeLimit, leftQueryIndex, rightQueryIndex, (cellReferenceIndex * 2 + 2));
+                if (indexFromLeftSubTreeSearch == -1) {
+                    return indexFromRightSubTreeSearch;
+                }
+                if (indexFromRightSubTreeSearch == -1) {
+                    return indexFromLeftSubTreeSearch;
+                }
+                if (elements[indexFromLeftSubTreeSearch] > elements[indexFromRightSubTreeSearch]) {
+                    return indexFromLeftSubTreeSearch;
+                }
+                return indexFromRightSubTreeSearch;
+            }
+
+
+            void buildSegmentTree() {
+                buildSegmentTreeRecursive(0, this.elements.length - 1, 0);
+            }
+
+            void buildSegmentTreeRecursive(int left, int right, int cellIndex) {
+                if (left == right) {
+                    segments[cellIndex] = left;
+                    return;
+                }
+                int leftChildIndex = cellIndex * 2 + 1;
+                int rightChildIndex = cellIndex * 2 + 2;
+                int mid = (left + right) / 2;
+                buildSegmentTreeRecursive(left, mid, leftChildIndex);
+                buildSegmentTreeRecursive(mid + 1, right, rightChildIndex);
+                if (elements[segments[leftChildIndex]] > elements[segments[rightChildIndex]]) {
+                    segments[cellIndex] = segments[leftChildIndex];
+                } else {
+                    segments[cellIndex] = segments[rightChildIndex];
+                }
+            }
+
+
+        }
+
+
+    }
+
+
+}
---
+++ src/test/java/interviewbit/solutions/InorderTraversalOfCartesianTreeTest.java
@@ -0,0 +1,72 @@
+package interviewbit.solutions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InorderTraversalOfCartesianTreeTest {
+
+    @Test
+    public void shouldCreateDistributedCartesianTree(){
+        InorderTraversalOfCartesianTree.TreeNode root = new InorderTraversalOfCartesianTree().solve(new int[]{10, 30, 7, 15, 5, 40, 19, 21, 35, 26, 17});
+        assertNotNull(root);
+        assertEquals(40, root.val);
+        assertEquals(30, root.left.val);
+        assertEquals(10, root.left.left.val);
+        assertNull(root.left.left.left);
+        assertNull(root.left.left.right);
+        assertEquals(15, root.left.right.val);
+        assertEquals(7, root.left.right.left.val);
+        assertEquals(5, root.left.right.right.val);
+        assertNull(root.left.right.left.left);
+        assertNull(root.left.right.left.right);
+        assertNull(root.left.right.right.left);
+        assertNull(root.left.right.right.right);
+        assertEquals(35, root.right.val);
+        assertEquals(21, root.right.left.val);
+        assertEquals(19, root.right.left.left.val);
+        assertNull(root.right.left.left.left);
+        assertNull(root.right.left.left.right);
+        assertNull(root.right.left.right);
+    }
+
+    @Test
+    public void shouldCreateLeftAlignedTree(){
+        InorderTraversalOfCartesianTree.TreeNode root = new InorderTraversalOfCartesianTree().solve(new int[]{1,2,3});
+        assertNotNull(root);
+        assertEquals(3, root.val);
+        assertEquals(2, root.left.val);
+        assertEquals(1, root.left.left.val);
+        assertNull(root.left.left.left);
+        assertNull(root.left.left.right);
+        assertNull(root.left.right);
+        assertNull(root.right);
+
+    }
+
+    @Test
+    public void shouldCreateRightAlignedTree(){
+        InorderTraversalOfCartesianTree.TreeNode root = new InorderTraversalOfCartesianTree().solve(new int[]{3,2,1});
+        assertNotNull(root);
+        assertEquals(3, root.val);
+        assertEquals(2, root.right.val);
+        assertEquals(1, root.right.right.val);
+        assertNull(root.right.right.left);
+        assertNull(root.right.right.right);
+        assertNull(root.right.left);
+        assertNull(root.left);
+    }
+
+
+    @Test
+    public void shouldCreateSingleNodeTree(){
+        InorderTraversalOfCartesianTree.TreeNode root = new InorderTraversalOfCartesianTree().solve(new int[]{3});
+        assertNotNull(root);
+        assertEquals(3, root.val);
+        assertNull(root.left);
+        assertNull(root.right);
+    }
+
+
+
+}
