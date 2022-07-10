086eee34226025493466d99788afe39330d53fdb
Rudrajyoti Biswas <rudrajyoti.biswas@outlook.com>
2022-07-10 14:54:18 +0530

    https://www.interviewbit.com/problems/sorted-array-to-balanced-bst/

---
+++ src/main/java/interviewbit/solutions/ConvertSortedArrayToBalancedBinarySearchTree.java
@@ -0,0 +1,50 @@
+package interviewbit.solutions;
+
+/*
+https://www.interviewbit.com/problems/sorted-array-to-balanced-bst/
+ */
+
+public class ConvertSortedArrayToBalancedBinarySearchTree {
+
+    public TreeNode solve(int[] sortedArray) {
+        return new Solution().sortedArrayToBST(sortedArray);
+    }
+
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+            left = null;
+            right = null;
+        }
+    }
+
+    @SuppressWarnings("InnerClassMayBeStatic")
+    public class Solution {
+        // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+        public TreeNode sortedArrayToBST(final int[] A) {
+            if (A == null || A.length == 0) {
+                return null;
+            }
+            return createBinarySearchTreeRecursive(0, A.length - 1, A);
+        }
+
+        private TreeNode createBinarySearchTreeRecursive(int startIndex, int endIndex, int[] sortedArray) {
+            if (startIndex > endIndex) {
+                return null;
+            }
+            int mid = (startIndex + endIndex) / 2;
+            TreeNode root = new TreeNode(sortedArray[mid]);
+            root.left = createBinarySearchTreeRecursive(startIndex, mid - 1, sortedArray);
+            root.right = createBinarySearchTreeRecursive(mid + 1, endIndex, sortedArray);
+            return root;
+        }
+
+    }
+
+
+}
---
+++ src/test/java/interviewbit/solutions/ConvertSortedArrayToBalancedBinarySearchTreeTest.java
@@ -0,0 +1,44 @@
+package interviewbit.solutions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConvertSortedArrayToBalancedBinarySearchTreeTest {
+
+    @Test
+    public void shouldCreateBalancedBinarySearchTree(){
+        ConvertSortedArrayToBalancedBinarySearchTree.TreeNode treeNode = new ConvertSortedArrayToBalancedBinarySearchTree().solve(
+                new int[]{1, 2, 3, 4, 5}
+        );
+        assertEquals(3, treeNode.val);
+        assertEquals(1, treeNode.left.val);
+        assertEquals(2, treeNode.left.right.val);
+        assertEquals(4, treeNode.right.val);
+        assertEquals(5, treeNode.right.right.val);
+        assertNull(treeNode.left.left);
+        assertNull(treeNode.left.right.left);
+        assertNull(treeNode.left.right.right);
+        assertNull(treeNode.right.left);
+        assertNull(treeNode.right.right.left);
+    }
+
+    @Test
+    public void shouldHandleNullArray(){
+        assertNull(new ConvertSortedArrayToBalancedBinarySearchTree().solve(null));
+    }
+
+    @Test
+    public void shouldHandleEmptyArray(){
+        assertNull(new ConvertSortedArrayToBalancedBinarySearchTree().solve(new int[]{}));
+    }
+
+    @Test
+    public void shouldHandleSingleSizeArray(){
+        ConvertSortedArrayToBalancedBinarySearchTree.TreeNode node = new ConvertSortedArrayToBalancedBinarySearchTree().solve(new int[]{1});
+        assertEquals(1, node.val);
+        assertNull(node.right);
+        assertNull(node.left);
+    }
+
+}
