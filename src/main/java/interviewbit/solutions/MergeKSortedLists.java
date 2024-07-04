ad29f5b4edfceede89cffb1b8c8ca4049dde72e6
Rudrajyoti Biswas <rudrajyoti.biswas@outlook.com>
2024-07-04 18:07:24 +0530

    Refactor

--- src/main/java/interviewbit/solutions/MergeKSortedLists.java
+++ src/main/java/interviewbit/solutions/MergeKSortedLists.java
@@ -7,21 +7,59 @@ https://www.interviewbit.com/problems/merge-k-sorted-lists/
 import ds.util.ListNode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class MergeKSortedLists {
 
-    public ListNode solve(ArrayList<ListNode> listNodes){
+    public ListNode solvePriorityQueue(ArrayList<ListNode> listNodes) {
+        return new Solution().mergeKListsPriorityQueue(listNodes);
+    }
+
+    public ListNode solveHeap(ArrayList<ListNode> listNodes) {
         return new Solution().mergeKLists(listNodes);
     }
 
     public static class Solution {
+
+        public ListNode mergeKListsPriorityQueue(ArrayList<ListNode> a) {
+            ListNode resultHead = null;
+            ListNode resultTail = null;
+
+            PriorityQueue<ListNode> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(o -> o.val));
+
+            for (ListNode node : a) {
+                priorityQueue.offer(node);
+            }
+
+            while (!priorityQueue.isEmpty()) {
+                ListNode minNode = priorityQueue.poll();
+                if (resultHead == null) {
+                    resultHead = minNode;
+                    resultTail = resultHead;
+                } else {
+                    resultTail.next = minNode;
+                    resultTail = resultTail.next;
+                }
+                if (minNode.next != null) {
+                    priorityQueue.offer(minNode.next);
+                }
+                resultTail.next = null;
+            }
+
+
+            return resultHead;
+
+        }
+
+
         public ListNode mergeKLists(ArrayList<ListNode> a) {
 
             HeapNode[] heapNodes = new HeapNode[a.size()];
             ListNode[] pointers = new ListNode[a.size()];
             int listsParsed = 0;
 
-            for (int i=0;i<a.size();i++){
+            for (int i = 0; i < a.size(); i++) {
                 heapNodes[i] = new HeapNode(i, a.get(i).val);
                 pointers[i] = a.get(i);
             }
@@ -30,19 +68,19 @@ public class MergeKSortedLists {
             ListNode resultHead = null;
             ListNode resultTail = null;
 
-            while (listsParsed < a.size()){
+            while (listsParsed < a.size()) {
                 HeapNode heapNode = minHeap.getMin();
-                if (resultHead == null){
+                if (resultHead == null) {
                     resultHead = pointers[heapNode.arrayIndex];
                     resultTail = resultHead;
-                }else{
+                } else {
                     resultTail.next = pointers[heapNode.arrayIndex];
                     resultTail = resultTail.next;
                 }
-                if (pointers[heapNode.arrayIndex].next == null){
+                if (pointers[heapNode.arrayIndex].next == null) {
                     ++listsParsed;
                     minHeap.replace(new HeapNode(heapNode.arrayIndex, Integer.MAX_VALUE));
-                }else{
+                } else {
                     pointers[heapNode.arrayIndex] = pointers[heapNode.arrayIndex].next;
                     minHeap.replace(new HeapNode(heapNode.arrayIndex, pointers[heapNode.arrayIndex].val));
                 }
@@ -52,7 +90,7 @@ public class MergeKSortedLists {
             return resultHead;
         }
 
-        private static class HeapNode{
+        private static class HeapNode {
             private final int arrayIndex;
             private final int value;
 
@@ -62,7 +100,7 @@ public class MergeKSortedLists {
             }
         }
 
-        private static class MinHeap{
+        private static class MinHeap {
             private final HeapNode[] elements;
 
             MinHeap(HeapNode[] elements) {
@@ -70,43 +108,44 @@ public class MergeKSortedLists {
                 heapify();
             }
 
-            public HeapNode getMin(){
+            public HeapNode getMin() {
                 return elements[0];
             }
 
-            public void replace(HeapNode heapNode){
+            public void replace(HeapNode heapNode) {
                 elements[0] = heapNode;
                 heapify();
             }
 
-            private void heapify(){
+            private void heapify() {
                 int elementsLength = elements.length;
-                int lastParentNode = (elementsLength-2) /2;
-                for (int i=lastParentNode;i>=0;i--){
+                int lastParentNode = (elementsLength - 2) / 2;
+                for (int i = lastParentNode; i >= 0; i--) {
                     minHeapify(i);
                 }
             }
 
-            private void minHeapify(int index){
-                int leftChild = 2*index+1;
-                int rightChild = 2*index + 2;
-                if (leftChild >= elements.length){
+            private void minHeapify(int index) {
+                int leftChild = 2 * index + 1;
+                int rightChild = 2 * index + 2;
+                if (leftChild >= elements.length) {
                     return;
                 }
-                if (rightChild >= elements.length){
-                    if (elements[index].value > elements[leftChild].value){
+                if (rightChild >= elements.length) {
+                    if (elements[index].value > elements[leftChild].value) {
                         swap(index, leftChild);
                         minHeapify(leftChild);
                     }
-                }else{
+                } else {
                     int swapIndex = elements[leftChild].value < elements[rightChild].value ? leftChild : rightChild;
-                    if (elements[index].value > elements[swapIndex].value){
+                    if (elements[index].value > elements[swapIndex].value) {
                         swap(index, swapIndex);
                         minHeapify(swapIndex);
                     }
                 }
             }
-            private void swap(int fromIndex, int toIndex){
+
+            private void swap(int fromIndex, int toIndex) {
                 HeapNode temp = elements[fromIndex];
                 elements[fromIndex] = elements[toIndex];
                 elements[toIndex] = temp;
@@ -115,7 +154,6 @@ public class MergeKSortedLists {
         }
 
     }
-
 
 
 }
--- src/test/java/interviewbit/solutions/MergeKSortedListsTest.java
+++ src/test/java/interviewbit/solutions/MergeKSortedListsTest.java
@@ -13,7 +13,27 @@ public class MergeKSortedListsTest {
     @Test
     public void shouldMergeLists() {
         assertArrayEquals(new Integer[]{1, 2, 3, 4, 6, 8, 12, 20, 25, 30, 40, 100, 500, 1001, 2000},
-                new MergeKSortedLists().solve(
+                new MergeKSortedLists().solvePriorityQueue(
+                        new ArrayList<>(
+                                List.of(new ListNode(1,
+                                                new ListNode(4,
+                                                        new ListNode(100))),
+                                        new ListNode(2,
+                                                new ListNode(40,
+                                                        new ListNode(1001))),
+                                        new ListNode(3,
+                                                new ListNode(12,
+                                                        new ListNode(500))),
+                                        new ListNode(6,
+                                                new ListNode(8,
+                                                        new ListNode(20))),
+                                        new ListNode(25,
+                                                new ListNode(30,
+                                                        new ListNode(2000))))
+                        )).toList().toArray(new Integer[0]));
+
+        assertArrayEquals(new Integer[]{1, 2, 3, 4, 6, 8, 12, 20, 25, 30, 40, 100, 500, 1001, 2000},
+                new MergeKSortedLists().solveHeap(
                         new ArrayList<>(
                                 List.of(new ListNode(1,
                                                 new ListNode(4,
@@ -36,7 +56,16 @@ public class MergeKSortedListsTest {
     @Test
     public void shouldMergeListOfSingleNodes(){
         assertArrayEquals(new Integer[]{1,2,3,5},
-                new MergeKSortedLists().solve(
+                new MergeKSortedLists().solveHeap(
+                        new ArrayList<>(
+                                List.of(new ListNode(1),
+                                        new ListNode(2),
+                                        new ListNode(5),
+                                        new ListNode(3))
+                        )
+                ).toList().toArray(new Integer[0]));
+        assertArrayEquals(new Integer[]{1,2,3,5},
+                new MergeKSortedLists().solvePriorityQueue(
                         new ArrayList<>(
                                 List.of(new ListNode(1),
                                         new ListNode(2),
